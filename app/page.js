3df3003924f4019e17ec14f0850a6c530b5522c6
Athif Saleem <athif.saleem1@gmail.com>
2025-03-11 20:35:52 -0700

    Stage 1: Added homepages, previous commit was from another file!!

--- app/page.js
+++ app/page.js
@@ -2,12 +2,17 @@
 export default function Home() {
   return (
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div className="mt-20 p-4">
       <p>Welcome!</p>
 =======
     <div className="mt-24">
       <p className="p-200">Welcome!</p>
 >>>>>>> 72ee9e12cdb9897870d84d788ca02d9fef55d295
+=======
+    <div className="mt-24">
+      <p className="p-200">Welcome!</p>
+>>>>>>> 083fbb4 (Stage 1: Added homepages, previous commit was from another file!!)
     </div>
   );
 }
